f2ab6eeb895313a190dfc8b8f125f182533a5756
Polystyreeni <jesperi.tolvanen@gmail.com>
2024-04-19 11:12:36 +0300

    Add zxcvbn password strength check

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
-import { isValidEmail, isValidPassword } from "../../utility/loginHelper";
+import { checkPasswordStrength, isValidEmail, isValidPassword } from "../../utility/loginHelper";
 import { registerThunk } from "../../store/actionCreators/thunks/Auth";
 import { AUTH_ROLE_TYPE } from "../../store/actionTypes";
 import { Navigate } from "react-router-dom";
 import styles from "./Register.module.css"
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
+import { Check, Dangerous, SentimentNeutralRounded, Warning } from "@mui/icons-material";
 
 const Register = () => {
 
@@ -15,6 +16,11 @@ const Register = () => {
     const [emailError, setEmailError] = useState(false);
     const [passError, setPassError] = useState(false);
     const [passRepeatError, setPassRepeatError] = useState(false);
+
+    // Password score checking
+    const [passScore, setPassScore] = useState(null);
+    const [passWarning, setPassWarning] = useState(null);
+    const [passTips, setPassTips] = useState([]);
 
     function onChangeEmail(e) {
         setEmailError(false);
@@ -42,7 +48,8 @@ const Register = () => {
             return;
         }
 
-        if (!isValidPassword(userData.password)) {
+        const passScoreValid = passScore === null ? checkPassScore(userData.password) : passScore > 3; 
+        if (!isValidPassword(userData.password) || !passScoreValid) {
             setPassError(true);
             return;
         }
@@ -53,6 +60,49 @@ const Register = () => {
         }
 
         dispatch(registerThunk(userData));
+    }
+
+    function onPasswordBlur(e) {
+        const pass = e.target.value;
+        checkPassScore(pass);
+    }
+
+    // Check password score
+    // Return true if password is strong
+    function checkPassScore(pass) {
+        if (pass === undefined || pass.length < 1)
+        {
+            setPassScore(null);
+            setPassWarning(null);
+            setPassTips([]);
+            return false;
+        }
+
+        const passResults = checkPasswordStrength(pass);
+        const feedback = passResults.feedback;
+        const score = passResults.score;
+
+        setPassScore(score);
+        setPassWarning(feedback.warning);
+        setPassTips(feedback.suggestions);
+
+        return score > 2;
+    }
+
+    function getPasswordLabel(score) {
+        switch (score) {
+            case 0:
+            case 1:
+                return (<Dangerous />)
+            case 2:
+                return (<Warning />)
+            case 3:
+                return (<SentimentNeutralRounded />)
+            case 4:
+                return (<Check />)
+            default:
+                return (<Check />)
+        }
     }
 
     return (
@@ -87,8 +137,21 @@ const Register = () => {
                         required
                         onChange={onChangePass}
                         error={passError}
+                        onBlur={onPasswordBlur}
                         helperText={passError ? "TODO PASSWORD RULE" : ""}/>
                 </div>
+                {passScore !== null && (
+                    <Stack alignItems="center" justifyContent="center" direction="row" gap={1}>
+                        <span>{getPasswordLabel(passScore)}</span>
+                        <Typography variant="body1">{`Password strength: ${passScore} / 4`}</Typography>
+                    </Stack>
+                )}
+                {passWarning !== null && (
+                    <Typography variant="body2">{passWarning}</Typography>
+                )}
+                {passTips.map((warning, index) => (
+                    <Typography key={index} variant="body2">{warning}</Typography>
+                ))}
                 <div className={styles.inputRow}>
                     <TextField
                             label="Repeat Password"
--- frontend/src/utility/loginHelper.js
+++ frontend/src/utility/loginHelper.js
@@ -1,5 +1,19 @@
+import { zxcvbn, zxcvbnOptions } from '@zxcvbn-ts/core'
+import * as zxcvbnCommonPackage from '@zxcvbn-ts/language-common'
+import * as zxcvbnEnPackage from '@zxcvbn-ts/language-en'
+
 export const validEmailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const passOptions = {
+    translations: zxcvbnEnPackage.translations,
+    graphs: zxcvbnCommonPackage.adjacencyGraphs,
+    dictionary: {
+        ...zxcvbnCommonPackage.dictionary,
+        ...zxcvbnEnPackage.dictionary
+    },
+}
+zxcvbnOptions.setOptions(passOptions);
 
 export const isValidEmail = (email) => {
     if (!validEmailRegex.test(email))
@@ -10,4 +24,8 @@ export const isValidEmail = (email) => {
 export const isValidPassword = (password) => {
     // TODO: For testing purposes only, no rules are enforced!!!
     return true;
+}
+
+export const checkPasswordStrength = (password) => {
+    return zxcvbn(password);
 }
