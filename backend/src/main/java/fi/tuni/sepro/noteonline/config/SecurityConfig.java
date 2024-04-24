d9da03068ad6d073d67e9d11dd435724d6285765
Polystyreeni <jesperi.tolvanen@gmail.com>
2024-04-24 21:54:29 +0300

    Add xss content security policy

--- backend/src/main/java/fi/tuni/sepro/noteonline/config/SecurityConfig.java
+++ backend/src/main/java/fi/tuni/sepro/noteonline/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -60,8 +61,10 @@ public class SecurityConfig {
         .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
-            auth -> auth.requestMatchers("/api/**").permitAll().anyRequest().authenticated()
-        );
+            auth -> auth.requestMatchers("/api/**").permitAll().anyRequest().authenticated())
+        .headers((headers) -> 
+            headers.xssProtection(Customizer.withDefaults())
+            .contentSecurityPolicy(config -> config.policyDirectives("default-src 'self'; form-action 'self'")));
 
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
