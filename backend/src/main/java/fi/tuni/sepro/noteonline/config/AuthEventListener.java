02d7713e1f59f89ffbff4fc9c2d2251dd3c9464b
Polystyreeni <jesperi.tolvanen@gmail.com>
2024-05-04 12:42:46 +0300

    Add login fail limits

---
+++ backend/src/main/java/fi/tuni/sepro/noteonline/config/AuthEventListener.java
@@ -0,0 +1,59 @@
+package fi.tuni.sepro.noteonline.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import fi.tuni.sepro.noteonline.models.User;
+import fi.tuni.sepro.noteonline.repository.UserRepository;
+
+@Component
+public class AuthEventListener {
+
+    @Value("${noteonline.app.accountLockMin}")
+    private int lockCountMin;
+
+    @Value("${noteonline.app.accountLockMax}")
+    private int lockCountHigh;
+
+    @Value("${noteonline.app.accountLockMinPenalty}")
+    private long lockDurationMin;
+
+    @Value("${noteonline.app.accountLockMaxPenalty}")
+    private long lockDurationHigh;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @EventListener
+    /**
+     * Listens for failed authentications, and disables further login attempts if too many failures
+     * occur
+     * @param event Failed authentication event
+     */
+    public void authenticationFailed(AuthenticationFailureBadCredentialsEvent event) {
+        String email = (String)event.getAuthentication().getPrincipal();
+
+        User user = userRepository.findUserByEmail(email);
+        if (user != null) {
+            int loginCount = user.getFailedLoginCount() + 1;
+            long lockedUntil = 0;
+
+            user.setFailedLoginCount(loginCount);
+            
+            if (loginCount >= lockCountHigh) {
+                lockedUntil = System.currentTimeMillis() + lockDurationHigh;
+            }
+            else if (loginCount >= lockCountMin) {
+                lockedUntil = System.currentTimeMillis() + lockDurationMin;
+            }
+
+            user.setFailedLoginCount(loginCount);
+            user.setLockedUntil(lockedUntil);
+
+            userRepository.save(user);
+        }
+    }
+}
